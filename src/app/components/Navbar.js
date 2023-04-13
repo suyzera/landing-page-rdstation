454f2f4f261a72dd97c5f2c9119ff3df05174bb7
Ussuy <ussuyart@gmail.com>
2023-04-13 16:45:52 -0300

    chore: adding navbar to layout

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <ul>
+      <li>Menu 1</li>
+      <li>Menu 2</li>
+      <li>Menu 3</li>
+      <li>Menu 4</li>
+      <li>Menu 5</li>
+    </ul>
+  );
+};
+
+export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body>{children}</body>
+    <html lang="en">
+      <head></head>
+      <body>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   );
 }
