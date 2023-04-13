90d203bbcd7d01b9d5d183c5c1525d58269ed050
Ussuy <ussuyart@gmail.com>
2023-04-13 19:28:03 -0300

    chore: creating array with color variables

---
+++ src/app/styleguide/variables/colors/index.js
@@ -0,0 +1,16 @@
+const colors = [
+  { name: "MidNight Green", value: "#0B485B" },
+  { name: "Battery Charged Blue", value: "#19C1CE" },
+  { name: "Bright Turquoise", value: "#07E5EB" },
+  { name: "Maximum Yellow Red", value: "#F2BF4E" },
+  { name: "Orange Yellow", value: "#F4CA6D" },
+  { name: "Black", value: "#000000" },
+  { name: "Dark Gun Metal", value: "#212429" },
+  { name: "Roman Silver", value: "#7E8A98" },
+  { name: "Manatee", value: "#97A1AC" },
+  { name: "Light Gray", value: "#CFD3D8" },
+  { name: "Anti Flash White", value: "#F1F3F5" },
+  { name: "White", value: "#FFFFFF" },
+];
+
+export default colors;
