17a741f7d34ae97e064d2157c311f43070a87ad3
Ussuy <ussuyart@gmail.com>
2023-04-17 19:51:19 -0300

    chore: creating assets page

---
+++ src/app/assets/icons.jsx
@@ -0,0 +1,43 @@
+import facebookIcon from "../images/icons/facebook.svg";
+import googleIcon from "../images/icons/google.svg";
+import hotmartIcon from "../images/icons/hotmart.svg";
+import icon from "../images/icons/icon.svg";
+import icon1 from "../images/icons/icon-1.svg";
+import icon2 from "../images/icons/icon-2.svg";
+import icon3 from "../images/icons/icon-3.svg";
+import icon4 from "../images/icons/icon-4.svg";
+import icon5 from "../images/icons/icon-5.svg";
+import instagramIcon from "../images/icons/instagram.svg";
+import linkedinIcon from "../images/icons/linkedin.svg";
+import pagseguroIcon from "../images/icons/pagseguro.svg";
+import rdStationCrmIcon from "../images/icons/rd-station-crm.svg";
+import salesforceIcon from "../images/icons/salesforce.svg";
+import shopifyIcon from "../images/icons/shopify.svg";
+import twitterIcon from "../images/icons/twitter.svg";
+import wordpressIcon from "../images/icons/wordpress.svg";
+import youtubeIcon from "../images/icons/youtube.svg";
+import zapierIcon from "../images/icons/zapier.svg";
+
+const Icons = [
+  icon,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  rdStationCrmIcon,
+  salesforceIcon,
+  googleIcon,
+  facebookIcon,
+  linkedinIcon,
+  wordpressIcon,
+  zapierIcon,
+  shopifyIcon,
+  hotmartIcon,
+  instagramIcon,
+  pagseguroIcon,
+  twitterIcon,
+  youtubeIcon,
+];
+
+export default Icons;
---
+++ src/app/assets/page.jsx
@@ -0,0 +1,58 @@
+import styles from "./Assets.module.scss";
+import Image from "next/image";
+import { Darker_Grotesque } from "next/font/google";
+import rdStationLogo from "../images/rd-station-cor-md.svg";
+import imgVideoThumbnail from "../images/img-video-thumbnail.svg";
+import imgRdoer from "../images/img-rdoer.svg";
+import HeadingLg from "../styleguide/components/Typograph/HeadingLg";
+import SubtitleSm from "../styleguide/components/Typograph/SubtitleSm";
+import Icons from "./icons";
+
+const darker = Darker_Grotesque({ subsets: ["latin"], weight: "900" });
+
+const Assets = () => {
+  return (
+    <div className={styles.grid}>
+      <section className={styles.assets}>
+        <h1 className={darker.className}>Assets</h1>
+      </section>
+
+      <section className={styles.logos}>
+        <HeadingLg content="Logo"></HeadingLg>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <Image src={rdStationLogo} />
+            <SubtitleSm content="logo-rd-station-default.svg"></SubtitleSm>
+          </div>
+          <div className={styles.logo}>
+            <Image src={rdStationLogo} />
+            <SubtitleSm content="logo-rd-station-default.svg"></SubtitleSm>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.images}>
+        <HeadingLg content="Logo"></HeadingLg>
+        <div className={styles.iconsContainer}>
+          {Icons.map((icon) => (
+            <div className={styles.icon}>
+              <Image src={icon} key={icon} />
+            </div>
+          ))}
+        </div>
+        <div className={styles.photosContainer}>
+          <div className={styles.photo}>
+            <Image src={imgRdoer} />
+            <span>img-results.jpg</span>
+          </div>
+          <div className={styles.photo}>
+            <Image src={imgVideoThumbnail} />
+            <span>img-video-thumbnail.jpg</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Assets;
