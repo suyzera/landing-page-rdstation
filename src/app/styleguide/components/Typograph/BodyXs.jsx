cce7b0e201fd7a8ce13fbafe464523967361bb33
Ussuy <ussuyart@gmail.com>
2023-04-17 16:27:11 -0300

    adjustment: removing padding from components

--- src/app/styleguide/components/Typograph/BodyXs.jsx
+++ src/app/styleguide/components/Typograph/BodyXs.jsx
@@ -3,11 +3,7 @@ import { Nunito_Sans } from "next/font/google";
 const nunito = Nunito_Sans({ subsets: ["latin"], weight: "400" });
 
 export const BodyXs = ({ content }) => {
-  return (
-    <p className={nunito.className} style={{ paddingBottom: "24px" }}>
-      {content}
-    </p>
-  );
+  return <p className={nunito.className}>{content}</p>;
 };
 
 export default BodyXs;
