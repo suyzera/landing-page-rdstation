586bc990c7d9b8a82b6d1ef9f6a7213f1bda9416
Ussuy <ussuyart@gmail.com>
2023-04-18 18:26:48 -0300

    chore: adding footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,6 +10,84 @@ import SubtitleSm from "./components/Typograph/SubtitleSm";
 import HighlightButton from "./components/Buttons/Highlight";
 import SecondaryButton from "./components/Buttons/Secondary";
 import PrimaryButton from "./components/Buttons/Primary";
+import rdStationCrmIcon from "./images/icons/rd-station-crm.svg";
+import salesforceIcon from "./images/icons/salesforce.svg";
+import googleIcon from "./images/icons/google.svg";
+import facebookIcon from "./images/icons/facebook.svg";
+import linkedinIcon from "./images/icons/linkedin.svg";
+import wordpressIcon from "./images/icons/wordpress.svg";
+import hotmartIcon from "./images/icons/hotmart.svg";
+import pagseguroIcon from "./images/icons/pagseguro.svg";
+import shopifyIcon from "./images/icons/shopify.svg";
+import zapierIcon from "./images/icons/zapier.svg";
+import twitterIcon from "./images/icons/twitter.svg";
+import instagramIcon from "./images/icons/instagram.svg";
+import youtubeIcon from "./images/icons/youtube.svg";
+import rdStationLogo from "./images/rd-station-cor-md.svg";
+import Image from "next/image";
+
+const icons = [
+  {
+    component: rdStationCrmIcon,
+    name: "RD Station CRM",
+  },
+  {
+    component: salesforceIcon,
+    name: "Salesforce",
+  },
+  {
+    component: googleIcon,
+    name: "Google",
+  },
+  {
+    component: facebookIcon,
+    name: "Facebook",
+  },
+  {
+    component: linkedinIcon,
+    name: "Linkedin",
+  },
+  {
+    component: wordpressIcon,
+    name: "Wordpress",
+  },
+  {
+    component: hotmartIcon,
+    name: "Hotmart",
+  },
+  {
+    component: pagseguroIcon,
+    name: "PagSeguro",
+  },
+  {
+    component: shopifyIcon,
+    name: "Shopify",
+  },
+  {
+    component: zapierIcon,
+    name: "Zapier",
+  },
+];
+
+const socialNetworkIcon = [
+  {
+    icon: twitterIcon,
+    url: "https://twitter.com/rd_station",
+  },
+  {
+    icon: facebookIcon,
+    url: "https://www.facebook.com/ResultadosDigitais/",
+  },
+  {
+    icon: linkedinIcon,
+    url: "https://www.linkedin.com/company/resultadosdigitais",
+  },
+  { icon: instagramIcon, url: "https://www.instagram.com/resdigitais/" },
+  {
+    icon: youtubeIcon,
+    url: "https://www.youtube.com/channel/UC5lPKvmw6-DFya_qreisENQ",
+  },
+];
 
 export default function Home() {
   return (
@@ -129,17 +207,41 @@ export default function Home() {
       </section>
 
       <section className={styles.operation}>
-        <HeadingXs>
+        <HeadingSm>
           Torne sua operação de Marketing ainda mais poderosa
-        </HeadingXs>
+        </HeadingSm>
         <BodyMd>
           O RD Station Marketing é um software para sua empresa fazer campanhas
           melhores, nutrir Leads, gerar oportunidades comerciais qualificadas e
           alcançar mais resultados.
         </BodyMd>
-        <div className={styles.icons}></div>
-        <PrimaryButton></PrimaryButton>
+        <div className={styles.icons}>
+          {icons.map((icon) => {
+            return (
+              <div className={styles.icon} key={icon.name}>
+                <Image src={icon.component}></Image>
+                <BodyXs>{icon.name}</BodyXs>
+              </div>
+            );
+          })}
+        </div>
+        <PrimaryButton>Criar conta gratuita</PrimaryButton>
       </section>
+
+      <footer className={styles.rdFooter}>
+        <Image className={styles.logo} src={rdStationLogo}></Image>
+        <hr className={styles.line}></hr>
+        <div className={styles.iconContainer}>
+          {socialNetworkIcon.map(({ icon, url }) => {
+            return (
+              <a href={url}>
+                <Image className={styles.icon} src={icon} key={url} />
+              </a>
+            );
+          })}
+        </div>
+        <BodyXs>© 2021 RD Station. Política de Privacidade.</BodyXs>
+      </footer>
     </div>
   );
 }
