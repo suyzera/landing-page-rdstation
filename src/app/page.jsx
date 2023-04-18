1a944e0216e4917daa6167fa686382e728a3e03c
Ussuy <ussuyart@gmail.com>
2023-04-17 23:06:50 -0300

    chore: adding new section to home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.scss";
 import Navbar from "./components/Navbar/Navbar";
 import BodyMd from "./components/Typograph/BodyMd";
 import HeadingMd from "./components/Typograph/HeadingMd";
+import HeadingXs from "./components/Typograph/HeadingXs";
 
 export default function Home() {
   return (
@@ -22,6 +23,23 @@ export default function Home() {
 
         <form className={styles.form} action="POST"></form>
       </main>
+
+      <section className={styles.rdMarketing}>
+        <div className={styles.rdMarketingContainer}>
+          <div className={styles.cut}></div>
+          <HeadingXs color="#FFF">
+            Estaremos juntos desde o primeiro passo!
+          </HeadingXs>
+          <BodyMd color="#FFF">
+            Se você ainda não sabe como extrair o máximo das ações digitais, não
+            tem problema. O RD Station Marketing é mais que uma ferramenta, nos
+            importamos com os seus resultados.
+          </BodyMd>
+          <BodyMd color="#FFF">
+            Estaremos com você do planejamento até a prática!
+          </BodyMd>
+        </div>
+      </section>
     </div>
   );
 }
