ff2e9f65c26de195b742eaccf36a930cdf111235
Ussuy <ussuyart@gmail.com>
2023-04-17 23:06:08 -0300

    adjustment: adding properties to component heading

--- src/app/components/Typograph/HeadingXs.jsx
+++ src/app/components/Typograph/HeadingXs.jsx
@@ -2,8 +2,12 @@ import { Darker_Grotesque } from "next/font/google";
 
 const darker = Darker_Grotesque({ subsets: ["latin"], weight: "700" });
 
-export const HeadingXs = ({ children }) => {
-  return <h5 className={darker.className}>{children}</h5>;
+export const HeadingXs = ({ children, color = "#000" }) => {
+  return (
+    <h5 style={{ color: color }} className={darker.className}>
+      {children}
+    </h5>
+  );
 };
 
 export default HeadingXs;
