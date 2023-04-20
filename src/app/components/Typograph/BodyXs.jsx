2c9df14aa41cbd9016d4596356a351fe4dacb783
Ussuy <ussuyart@gmail.com>
2023-04-19 21:52:50 -0300

    adjustment: adding rules to heading component

--- src/app/components/Typograph/BodyXs.jsx
+++ src/app/components/Typograph/BodyXs.jsx
@@ -2,8 +2,12 @@ import { Nunito_Sans } from "next/font/google";
 
 const nunito = Nunito_Sans({ subsets: ["latin"], weight: "400" });
 
-export const BodyXs = ({ children }) => {
-  return <p className={nunito.className}>{children}</p>;
+export const BodyXs = ({ children, color = "black" }) => {
+  return (
+    <p style={{ color: color }} className={nunito.className}>
+      {children}
+    </p>
+  );
 };
 
 export default BodyXs;
